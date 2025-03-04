56d52e1c9e3a775fb805a954476a6addd9e659d3
suliamn2772002 <sul2772002@gmail.com>
2025-03-05 00:08:15 +0200

    fifth commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,40 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function index(){
-        $tasks = DB::table('tasks')->get();
+        // $tasks = DB::table('tasks')->get();
+        $tasks = Task::all();
         return view('tasks', compact('tasks'));
     }
 
     public function create(){
-        DB::table('tasks')->insert([
-            'name' => $_POST['name'],
-        ]);
+        // DB::table('tasks')->insert([
+        //     'name' => $_POST['name'],
+        // ]);
+        $task = new Task();
+        $task->name = $_POST['name'];
+        $task->save();
         return redirect()->back();
     }
 
     public function destroy ($id){
         // DB::table('tasks')->delete($id);
-        DB::table('tasks')->where('id' ,$id)->delete();
+        // DB::table('tasks')->where('id' ,$id)->delete();
+        $task = Task::find($id);
+        $task->delete();
         return redirect()->back();
     }
 
     public function edit($id){
-        $task = DB::table('tasks')->where('id' , $id)->first();
-        $tasks = DB::table('tasks')->get();
+        // $task = DB::table('tasks')->where('id' , $id)->first();
+        $task = Task::find($id);
+        // $tasks = DB::table('tasks')->get();
+        $tasks = Task::all();
         return view('tasks' , compact("task" , "tasks"));
     }
 
     public  function update(){
-        DB::table('tasks')->where("id" , $_POST['id'])->update([
-            "name" => $_POST['name']
-        ]);
-
+        // DB::table('tasks')->where("id" , $_POST['id'])->update([
+        //     "name" => $_POST['name']
+        // ]);
+        $task = Task::find($_POST['id']);
+        $task->name = $_POST['name'];
+        $task->save();
         return redirect('/tasks');
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,43 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index(){
-        $users = DB::table('users')->get();
+        // $users = DB::table('users')->get();
+        $users = User::all();
         return view('users', compact('users'));
     }
 
     public function create(){
-        DB::table('users')->insert([
-            'name' => $_POST['name'],
-            'email' => $_POST['email'],
-            'password' => $_POST['password'],
-        ]);
+        // DB::table('users')->insert([
+        //     'name' => $_POST['name'],
+        //     'email' => $_POST['email'],
+        //     'password' => $_POST['password'],
+        // ]);
+        $user = new User();
+        $user->name = $_POST['name'];
+        $user->email = $_POST['email'];
+        $user->password = $_POST['password'];
+        $user->save();
         return redirect()->back();
     }
 
     public function destroy ($id){
-        DB::table('users')->where('id' ,$id)->delete();
+        // DB::table('users')->where('id' ,$id)->delete();
+        $user = User::find($id);
+        $user->delete();
         return redirect()->back();
     }
 
     public function edit($id){
-        $user = DB::table('users')->where('id' , $id)->first();
-        $users = DB::table('users')->get();
+        // $user = DB::table('users')->where('id' , $id)->first();
+        $user = User::find($id);
+        // $users = DB::table('users')->get();
+        $users = User::all();
         return view('users' , compact("user" , "users"));
     }
 
     public  function update(){
-        DB::table('users')->where("id" , $_POST['id'])->update([
-            'name' => $_POST['name'],
-            'email' => $_POST['email'],
-            'password' => $_POST['password'],
-        ]);
-
+        // DB::table('users')->where("id" , $_POST['id'])->update([
+        //     'name' => $_POST['name'],
+        //     'email' => $_POST['email'],
+        //     'password' => $_POST['password'],
+        // ]);
+        $user = User::find($_POST['id']);
+        $user->name = $_POST['name'];
+        $user->email = $_POST['email'];
+        $user->password = $_POST['password'];
+        $user->save();
         return redirect('/users');
     }
 }
